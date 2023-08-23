f41cc591a1eb0b00aee13e82dec41ef46c1c2576
Clive Sasaka <clivesasaka@gmail.com>
2023-08-23 16:23:49 +0300

    Add basic retrival and insertion cache module

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Basic caching module"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic cache class that allows storing and
+    retrieving items from a dictionary."""
+    def put(self, key, item):
+        """A method that adds an item to the cache"""
+        if key is None or item is None:
+            return
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """A method that gets an item by key"""
+        return self.cache_data.get(key, None)
